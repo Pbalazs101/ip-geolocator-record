584f73c4f5750c818f4cf92a0a2c2c737f47673d
Balázs Pintér <pbalazs101@gmail.com>
2021-03-04 23:14:12 +0100

    Deleting GeoLocation.java from src/main/java/

--- src/main/java/GeoLocation.java
+++
@@ -1,9 +0,0 @@
-package geolocator;
-
-/**
- * Class for wrapping geolocation of an IP address.
- */
-
- import java.lang.Record;
-
- record GeoLocation(String as, String city, String country, String countryCode, String isp, double lat, double lon, String org, String query, String region, String regionName, String timezone, String zip){}
